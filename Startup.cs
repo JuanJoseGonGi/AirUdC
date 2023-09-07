d7b07af56d661050fcb3301ad186181fffe91b4b
Juan José González Giraldo <juanj@DESKTOP-JJGG>
2023-09-07 11:56:08 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirUdC.Startup))]
+namespace AirUdC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
